84c9c18075ecbe4df69dfded5dd6af7b05739482
muktaking <yeakubinfire@gmail.com>
2025-03-31 21:31:16 +0600

    data folder added

---
+++ app/scale/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Scale() {
+  return (
+    <div>
+      <p className="text-lg font-bold">Scale Name: Beck Scale for Suicidal</p>
+      <p>January 2013</p>
+      <div>
+        <p>Authors:</p>
+        <div></div>
+      </div>
+      <div>Tags</div>
+      <div>Publication link</div>
+    </div>
+  );
+}
